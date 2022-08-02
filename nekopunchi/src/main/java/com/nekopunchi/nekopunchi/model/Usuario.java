782aea474e0d6b933268d4a280f50f7848f79ff4
Luiz Guilherme Nycholas <L.GuiNycholas2000@gmail.com>
2022-08-02 01:11:31 -0300

    Integração com BD
    Ajustado Service e Repository para executar os comandos CRUD na base do PostgreSQL.

--- nekopunchi/src/main/java/com/nekopunchi/nekopunchi/model/Usuario.java
+++ nekopunchi/src/main/java/com/nekopunchi/nekopunchi/model/Usuario.java
@@ -1,10 +1,17 @@
 package com.nekopunchi.nekopunchi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
 
 //#region Atributos
+    @Id //Vai tranformar essa coluna em uma PK
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    
     private String nome;
 //#endregion
 
--- nekopunchi/src/main/java/com/nekopunchi/nekopunchi/repository/UsuarioRepository.java
+++ nekopunchi/src/main/java/com/nekopunchi/nekopunchi/repository/UsuarioRepository.java
@@ -1,81 +1,10 @@
 package com.nekopunchi.nekopunchi.repository;
+import com.nekopunchi.nekopunchi.model.Usuario;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.nekopunchi.nekopunchi.model.Usuario;
-import com.nekopunchi.nekopunchi.model.exception.ResourceNotFoundException;
-
 @Repository
-public class UsuarioRepository {
-
-    private List<Usuario> usuarios = new ArrayList<Usuario>();
-    private Integer ultimoId = 0;
-
-    /**
-     * Método para retornar uma lista de usuários.
-     * @return lista de usuários.
-     */
-    public List<Usuario> obterUsuarios(){
-        return usuarios;
-    }
-
-    /**
-     * Método que retorna uma lista de usuários.
-     * @param id do usuário que será localizado.
-     * @return Retorna um usuário caso tenha encontrado.
-     */
-    public Optional<Usuario> obterPorId(Integer id){
-        return usuarios
-            .stream()
-            .filter(usuario -> usuario.getId() == id)
-            .findFirst();
-    }
-
-    /**
-     * Método para adicionar usuário na lista.
-     * @param usuario que será adicionado.
-     * @return Retorna o produto que foi adicionado na lista.
-     */
-    public Usuario adicionar(Usuario usuario){
-        
-        ultimoId ++;
-        usuario.setId(ultimoId);
-        usuarios.add(usuario);
-        return usuario;
-    } 
-
-    /**
-     * Método para deletar o usuário por id.
-     * @param id do usuário que será deletado.
-     */
-    public void deletar (Integer id){
-        usuarios.removeIf(usuario -> usuario.getId() == id);
-    }
-
-    /**
-     * Método para atualizar o usuário na lista.
-     * @param id usuario que será atualizado.
-     * @return Retorna o usuário após atualizar a lista.
-     */
-    public Usuario atualizar (Usuario usuario){
-
-        //Encontrar o usuário na lista:
-        Optional<Usuario> usuarioEncontrado = obterPorId(usuario.getId());
-
-        if (usuarioEncontrado.isEmpty()){
-            throw new ResourceNotFoundException("Usuário não encontrado");
-        }
-        
-        //Remover o usuário antigo da lista:
-        deletar(usuario.getId());
-
-        //Adicionar o usuário atualizado na lista:    
-        usuarios.add(usuario);
-        
-        return usuario;
-    }
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+    
 }
